1a82ac36ad08bc6d0433fb3c2ebfcde3da68cf15
Cyrus <hoangminh0268@Gmail.com>
2023-05-04 19:00:53 +1000

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar className="navbar-expand-lg" sticky="top" color="dark" dark >
+        <NavbarBrand href="/" className="me-auto">
+          230MOVIES
+        </NavbarBrand>
+        <NavbarToggler onClick={() => setCollapsed(!collapsed)} className="me-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav className="container-fluid" navbar>
+            <NavItem>
+              <NavLink>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Movies</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Register</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Login</NavLink>
+            </NavItem>
+            <NavItem className="ml-auto">
+              <NavLink>USERNAME</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
